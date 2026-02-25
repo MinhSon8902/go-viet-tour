8f9891c2fa2b2f4f4c0e9a2802624a6ba32e90d9
Minh Son <minhson892002@gmail.com>
2026-02-25 23:00:22 +0700

    Initial commit: project setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,47 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from 'eslint-config-prettier/flat'
+import importPlugin from 'eslint-plugin-import';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  {
+    plugins: {
+      "import": importPlugin,
+    },
+    settings: {
+      "import/resolver": {
+        typescript: true,
+      },
+    },
+    rules: {
+      "import/no-duplicates": "error",
+      "import/no-named-as-default": "warn",
+      "import/no-named-as-default-member": "warn",
+      "import/newline-after-import": ["error", { "count": 1 }],
+      "import/no-anonymous-default-export": "warn",
+
+      "import/order": [
+        "error",
+        {
+          "groups": ["builtin", "external", "internal", "parent", "sibling", "index", "type"],
+          "alphabetize": {
+            "order": "asc",
+            "caseInsensitive": true
+          },
+          pathGroupsExcludedImportTypes: ["type"],
+        }
+      ],
+
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" }
+      ]
+    }
+  },
+  prettier,
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
